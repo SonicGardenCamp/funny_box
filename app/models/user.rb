0a79ae6e15f8ea13226fec327a0579160eb28286
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-28 05:39:00 +0000

    ADD_User_model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase.strip }
+  validates :name,  presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+end
