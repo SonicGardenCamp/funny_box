407d87fecd64147e0d0df8427021d2fc717c6c0f
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-28 07:35:31 +0000

    Fin_Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
