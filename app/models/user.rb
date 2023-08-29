02ca46fc2515f5d31c7ca5c50e3753a53b7ce0d3
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-29 04:43:14 +0000

    editpage_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :user_group_relationships
   has_many :groups, through: :user_group_relationships
- 
+
   before_save { self.email = email.downcase.strip }
   validates :name,  presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,5 +10,5 @@ class User < ApplicationRecord
                     uniqueness: true
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
