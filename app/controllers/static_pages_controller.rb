00717961b1d07afc3c72c1f7fe1cbf7a2fd527d9
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-08-31 04:09:54 +0000

    homeのレイアウト

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @groups = Group.all
+    @groups = Group.includes(:posts)
+    @groups = Group.joins(:posts).order('posts.created_at DESC')
   end
 end
