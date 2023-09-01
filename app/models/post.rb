a88d24081502b86e4fa344875eece5d12cd1f484
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-09-01 07:07:19 +0000

    検索機能実装(軽微なバグ未解決)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[content]
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   has_many :tag_group_relationships
   has_many :groups, through: :tag_group_relationships
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name, genre]
+  end
+
 end
