8f5a9723882419c56f7bcd8cc5327e14491b4ceb
Kiji0517 <ky9_mr30@yahoo.co.jp>
2023-09-04 05:33:17 +0000

    ホーム画面のエラーの修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @groups = Group.includes(:posts).joins(:posts).order('posts.created_at DESC').first(3)
+    @groups = Group.includes(:posts).joins(:posts).order(last_posted_at: :desc).first(3)
   end
 end
