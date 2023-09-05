dfbfa32e8fb27a0fd0177bceaf3d713b73d13aca
SanaRyu <sanadaryuusei@gmail.com>
2023-09-05 07:34:42 +0000

    微調整

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :tag_group_relationships, dependent: :delete_all
+  has_many :tag_group_relationships, dependent: :destroy
   has_many :groups, through: :tag_group_relationships
 
   def self.ransackable_attributes(auth_object = nil)
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,54 +4,54 @@ user1 = User.create!(name:  "ぐるくん皇帝",
             password_confirmation: "gurukun")
 
 
-# group1 = Group.create!(name: "ぐるくん後援会北京支部",
-#               description: "愚瑠君最高！",
-#               category: "ぐるくん",
-#               host_user_id: 1)
+group1 = Group.create!(name: "ぐるくん後援会北京支部",
+              description: "愚瑠君最高！",
+              category: "ぐるくん",
+              host_user_id: 1)
 
-# group2 = Group.create!(name: "ぐるくん後援会大阪支部",
-#               description: "ぐるくんは最高や！",
-#               category: "ぐるくん",
-#               host_user_id: 1)
+group2 = Group.create!(name: "ぐるくん後援会大阪支部",
+              description: "ぐるくんは最高や！",
+              category: "ぐるくん",
+              host_user_id: 1)
 
-# group3 = Group.create!(name: "ぐるくん後援会アラスカ支部",
-#               description: "Gurukun is wonderful!!",
-#               category: "ぐるくん",
-#               host_user_id: 1)
+group3 = Group.create!(name: "ぐるくん後援会アラスカ支部",
+              description: "Gurukun is wonderful!!",
+              category: "ぐるくん",
+              host_user_id: 1)
 
-# group1.users << user1              
-# group2.users << user1              
-# group3.users << user1
+group1.users << user1              
+group2.users << user1              
+group3.users << user1
 
-# 10.times do |m|
-#   group = Group.create(name: Faker::Hobby.activity,
-#                         host_user_id: 1,
-#                         category: Group::CATEGORIES[rand(Group::CATEGORIES.length)],
-#                         )
+10.times do |m|
+  group = Group.create(name: Faker::Hobby.activity,
+                        host_user_id: 1,
+                        category: Group::CATEGORIES[rand(Group::CATEGORIES.length)],
+                        )
 
-#   3.times do |n|
-#     name  = Faker::Name.name
-#     email = "example-#{n+1+(3*m)}@railstutorial.org"
-#     password = "password"
-#     user = User.create(name:  name,
-#                         email: email,
-#                         password:              password,
-#                         password_confirmation: password)
-#     group.users << user
+  3.times do |n|
+    name  = Faker::Name.name
+    email = "example-#{n+1+(3*m)}@railstutorial.org"
+    password = "password"
+    user = User.create(name:  name,
+                        email: email,
+                        password:              password,
+                        password_confirmation: password)
+    group.users << user
 
-#     2.times do
-#       post = user.posts.create(content: Faker::Lorem.sentence(word_count: 5))
-#       group.posts << post
-#     end
-#   end
+    2.times do
+      post = user.posts.create(content: Faker::Lorem.sentence(word_count: 5))
+      group.posts << post
+    end
+  end
   
-#   group.users << user1
+  group.users << user1
   
-#   # グループに関連するタグを生成
-#   3.times do
-#     tag = Tag.create(name: Faker::Lorem.word)
-#     group.tags << tag
-#   end
+  # グループに関連するタグを生成
+  3.times do
+    tag = Tag.create(name: Faker::Lorem.word)
+    group.tags << tag
+  end
 
-# end
+end
 
