0b337f4402265947da267888b114611ffc205650
kiharatadahiro <kihara@sonicgarden.jp>
2023-09-05 18:15:31 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   resources :users
   resources :groups do
     resources :posts, only: [:create, :destroy]
+    resources :tags, only: %i[create], module: :groups
   end
   post "enter_leave/:id",   to: "groups#enter_leave", as: "enter_leave"
-  post "create_tag/:id",   to: "groups#create_tag", as: "create_tag"
+
   resource :capsule_toys, only: [:show]
 end
