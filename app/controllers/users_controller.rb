c0ce0d61fe3ffa593ddc9259187b02869f5d53cf
fuku <fuku@sonicgarden.jp>
2023-09-06 08:18:02 +0000

    usersコントローラリファクタ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:edit, :update]
-  before_action :correct_user, only:[:edit, :update, :destroy]
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update destroy]
+  before_action :set_users, only: %i[show edit update destroy]
 
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
-
     # TODO: 仮実装
     @groups = @user.groups.first(3)
 
@@ -27,11 +26,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       # if @user.changed
         flash[:success] = "プロフィールが更新されました"
@@ -43,12 +41,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    # User.find(params[:id]).destroy
+    @user.destroy
     flash[:success] = "ユーザーを削除しました"
     redirect_to root_path
   end
 
   private
+  
+    def set_users
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation,
