3556257f448a35bd64d3938d09693dac10e60588
fuku <fuku@sonicgarden.jp>
2023-09-06 08:05:19 +0000

    routingのリファクタを修正した

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
 
-  resources :users, only: %i[new, create, show, edit, update, destroy]
+  resources :users, only: %i[new create show edit update destroy]
   resources :groups do
-    resources :posts, only: %i[create, destroy]
+    resources :posts, only: %i[create destroy]
     resources :tags, only: %i[create], module: :groups
   end
   post "enter_leave/:id",   to: "groups#enter_leave", as: "enter_leave"
